376714a87bf9474ae8939d5d249b11e4285b3e16
Bhargav Parashar <bhargavparashar99@gmail.com>
2024-12-24 23:51:45 +0530

    Added validations in blogSchema

--- models/blog.model.js
+++ models/blog.model.js
@@ -2,10 +2,10 @@
 const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema({
-    title: String, //Title is string
-    authors: [String], //Authors is an array of strings
-    content: String, //Content is string
-    publishedAt: Date, //publishedAt is Date  
+    title: {type: String, required:true}, //Title is string
+    authors:{type: [String] }, //Authors is an array of strings
+    content: {type: String,default:""}, //Content is string
+    publishedAt: {type:Date,default:null}, //publishedAt is Date  
 });
 
 const blogModel = mongoose.model("Blog",blogSchema); //Name of model, schema, collection. If name of collection is not provided, by default an S is added to the name of the model and that is taken as the collection name.
