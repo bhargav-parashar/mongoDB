b7877b4b82bee01ff7aadd8cdb7cb6feb6a118ce
Bhargav Parashar <bhargavparashar99@gmail.com>
2024-12-25 15:53:15 +0530

    Added Delete By Id Endpoint

--- controllers/blogs.controllers.js
+++ controllers/blogs.controllers.js
@@ -15,7 +15,7 @@ const createBlog = async (req, res) => {
         res.status(201).send(newBlog); //201 : created
     }catch(err){
         if(err.code === 11000)
-            return res.status(409).send({message:"A blog with this title already exists."});
+            return res.status(409).send({message:"A blog with this title already exists."}); //409: conflict detected in client request
         res.status(500).send({message:"Something went wrong!", err});
     }
 };
@@ -54,5 +54,16 @@ const updateBlogById = async (req,res) =>{
      }
 }
 
+const deleteBlogById = async (req,res) =>{
+    try{
+        const {blogId} = req.params;
+        await Blog.findByIdAndDelete(blogId,req.body);
+        res.sendStatus(204); //204: The request that you made was success, there is nothing to send back to you.
+     }catch(err){
+         console.log(err);
+         res.status(500).send({message:"Something went wrong!", err});
+     }
+}
 
-module.exports = {createBlog,getAllBlogs,getBlogById,updateBlogById};
+
+module.exports = {createBlog,getAllBlogs,getBlogById,updateBlogById,deleteBlogById};
--- routes/blogs.routes.js
+++ routes/blogs.routes.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
-const {createBlog, getAllBlogs,getBlogById,updateBlogById} = require("../controllers/blogs.controllers"); 
+const {createBlog, getAllBlogs,getBlogById,updateBlogById,deleteBlogById} = require("../controllers/blogs.controllers"); 
 
 router.post("/new", createBlog);
 router.get("/",getAllBlogs);
 router.get("/:blogId", getBlogById);
 router.patch("/:blogId", updateBlogById);
+router.delete("/:blogId",deleteBlogById);
 
 module.exports = router;
